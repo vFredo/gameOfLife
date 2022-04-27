6c5c04baa456ef20cbe476815dc4e28bace50d1a
Fredo <luisalfredo9905@gmail.com>
2022-04-27 13:38:17 -0500

    feat: format and adding logic for creating a preset

--- model/preset.go
+++ model/preset.go
@@ -2,9 +2,13 @@ package model
 
 import (
 	"encoding/json"
-  "io/ioutil"
+	"io/ioutil"
 	"log"
+	"math"
+	"os"
 )
+
+const PRESET_FILE = "./presets/"
 
 type Preset struct {
 	Name       string   `json:"name"`
@@ -19,32 +23,74 @@ type PresetManager struct {
 
 // Load every preset that is on the './presets/' folder
 func (pm *PresetManager) FetchPresets() {
-  files, err := ioutil.ReadDir("./presets/")
-  if err != nil {
-    log.Fatal("Can't read folder presets: ", err)
-  }
-  for _, file := range files {
-    content, err := ioutil.ReadFile("./presets/" + file.Name())
-    if err != nil {
-      log.Fatal("Error when openning files: ", err)
-    }
+	files, err := ioutil.ReadDir(PRESET_FILE)
+	if err != nil {
+		log.Fatalf("Can't read folder presets: %s", err)
+	}
+	for _, file := range files {
+		content, err := ioutil.ReadFile(PRESET_FILE + file.Name())
+		if err != nil {
+			log.Fatalf("Error when openning file '%s': %s", file.Name(), err)
+		}
 
-    var newPreset Preset
-    err = newPreset.DecodeFromJson(content)
-    if err != nil {
-      log.Fatal("Error during decoding from json to preset: ", err)
-    }
-    pm.Presets = append(pm.Presets, newPreset)
-  }
+		var newPreset Preset
+		err = newPreset.DecodeFromJson(content)
+		if err != nil {
+			log.Fatalf("Error during decoding from json to preset: %s", err)
+		}
+		pm.Presets = append(pm.Presets, newPreset)
+	}
+}
+
+// TODO: Need to test this
+func (pm *PresetManager) CreatePreset(name string, board []uint8, x int, y int) {
+	max_width, max_height := math.Inf(-1), math.Inf(-1)
+	min_width, min_height := math.Inf(1), math.Inf(1)
+	var newPreset Preset
+	newPreset.Name = name
+
+	for i := x - 1; i > 0; i-- {
+		for j := y - 1; j > 0; j-- {
+			pos := (i * y) + j
+			if (board[pos] & 0x01) == 0x01 {
+				max_width = math.Max(max_width, float64(i))
+				min_width = math.Min(min_width, float64(i))
+
+				max_height = math.Max(max_height, float64(j))
+				min_height = math.Min(min_height, float64(j))
+				newPreset.AliveCells = append(newPreset.AliveCells, []uint{uint(i), uint(j)})
+			}
+		}
+	}
+
+	newPreset.Width = uint(max_width - min_width)
+	newPreset.Height = uint(max_height - min_height)
+
+	for i := 0; i < len(newPreset.AliveCells); i++ {
+		newPreset.AliveCells[i][0] -= newPreset.Width
+		newPreset.AliveCells[i][1] -= newPreset.Height
+	}
+
+	newPreset.EncodeToJson()
+	pm.Presets = append(pm.Presets, newPreset)
 }
 
 // Encode struct Preset into a json string
-func (p *Preset) EncodeToJson() ([]byte, error) {
+func (p *Preset) EncodeToJson() {
+	// MarshalIndent to format the output
 	encodedPreset, err := json.Marshal(p)
 	if err != nil {
 		log.Fatalf("Error occurred encoding struct Preset: %s", err.Error())
 	}
-	return encodedPreset, nil
+
+	file, err := os.OpenFile(PRESET_FILE+p.Name+".json", os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatalf("Error occurred while open/created file %s: %s", p.Name, err)
+	}
+	defer file.Close()
+
+	file.Write(encodedPreset)
+	file.Close()
 }
 
 // Decode a json string into struct Preset
